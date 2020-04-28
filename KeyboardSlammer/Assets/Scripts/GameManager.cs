a144d06ba97d47f180f70984724d35ed7983d6e6
Kevin <kevin.ka_96@hotmail.com>
2020-04-28 19:07:30 +0200

    First things first

---
+++ KeyboardSlammer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text moneyText;
+    public static int globalCash;
+
+    private UpgradesManager upManager;
+
+    private float _waitSeconds;
+
+    private void Start()
+    {
+        upManager = FindObjectOfType<UpgradesManager>();
+    }
+
+    void Update()
+    {
+        KeyPresses();
+    }
+
+    void KeyPresses()
+    {
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+            {
+                Debug.Log(kcode);
+                globalCash++;
+            }
+        }
+        moneyText.text = "€ " + globalCash;
+
+        if (globalCash > 499)
+        {
+            upManager.Upgrade1(true);
+        }
+    }
+}
---
+++ KeyboardSlammer/Assets/Scripts/UpgradesManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradesManager : MonoBehaviour
+{
+    public Button upgrade1Btn, upgrade2Btn, upgrade3Btn;
+
+    public void Upgrade1(bool upgrade)
+    {
+        if (upgrade)
+        {
+            upgrade1Btn.interactable = true;
+        }
+    }
+
+    public void FirstUpgrade()
+    {
+        GameManager.globalCash -= 500;
+        Upgrade1(false);
+    }
+}
