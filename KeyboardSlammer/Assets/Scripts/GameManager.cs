56a8dd7229471db64ba57a44692633db7e65c304
Kevin <kevin.ka_96@hotmail.com>
2020-05-13 18:40:51 +0200

    Codidoo

--- KeyboardSlammer/Assets/Scripts/GameManager.cs
+++ KeyboardSlammer/Assets/Scripts/GameManager.cs
@@ -3,41 +3,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public Text moneyText;
-    public static int globalCash;
+    public static double internalCash = 0;
 
     private UpgradesManager upManager;
 
-    private float _waitSeconds;
+    private double _keyPressScore = 10;
+    private int oneK = 1000;
+    private int oneM = 1000000;
+    private int oneB = 1000000000;
+    string thousand = "k";
+    string million = "m";
+    string displayString;
 
     private void Start()
     {
         upManager = FindObjectOfType<UpgradesManager>();
+        displayString = "€ " + internalCash.ToString();
     }
 
     void Update()
     {
         KeyPresses();
+        DisplayMoney(displayString);
+        
     }
 
-    void KeyPresses()
+    void DisplayMoney(string moneyString)
     {
+        moneyText.text = moneyString;
+    }
+
+    void MoneyCounter(double money)
+    {
+        internalCash += money;
+        Debug.Log(internalCash);
+        var moneyDisplayString1 = "€ " + (internalCash / oneK).ToString("0.##") + thousand;
+        var moneyDisplasString2 = "€ " + (internalCash / oneM).ToString("0.##") + million;
+        var moneyDisplayString3 = "€ " + (internalCash / oneB).ToString("0.##") + "aa";
+
+        if (internalCash > 9999)
+        {
+            displayString = moneyDisplayString1;
+            if (internalCash > 999999)
+            {
+                displayString = moneyDisplasString2;
+                if (internalCash > 9999999999)
+                {
+                    displayString = moneyDisplayString3;
+                }                
+            }            
+        }
+        else
+            displayString = "€ " + internalCash.ToString();
+    }
+
+    public void KeyPresses()
+    {
+        //var display = displayString;
+        //DisplayMoney(display);
         foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
         {
-            if (Input.GetKeyDown(kcode))
+            if (Input.GetKeyDown(kcode) == Input.GetMouseButton(0) || Input.GetKeyDown(kcode) == Input.GetMouseButton(1))
             {
-                Debug.Log(kcode);
-                globalCash++;
+                continue;                        
             }
-        }
-        moneyText.text = "€ " + globalCash;
-
-        if (globalCash > 499)
-        {
-            upManager.Upgrade1(true);
+            else
+                MoneyCounter(_keyPressScore);
         }
     }
 }
--- KeyboardSlammer/Assets/Scripts/UpgradesManager.cs
+++ KeyboardSlammer/Assets/Scripts/UpgradesManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,18 +7,35 @@ using UnityEngine.UI;
 public class UpgradesManager : MonoBehaviour
 {
     public Button upgrade1Btn, upgrade2Btn, upgrade3Btn;
+    private GameManager _gm;
+    private double _upgradeCost = 500;
+    private double _upgradeFloat = 1.005;
+
+    private void Awake()
+    {
+        _gm = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (GameManager.internalCash > _upgradeCost)
+            Upgrade1(true);
+    }
 
     public void Upgrade1(bool upgrade)
     {
-        if (upgrade)
-        {
-            upgrade1Btn.interactable = true;
-        }
+        if (upgrade)        
+            upgrade1Btn.interactable = true;        
+        else if (!upgrade)
+            upgrade1Btn.interactable = false;
     }
 
-    public void FirstUpgrade()
+    public void Upgrade()
     {
-        GameManager.globalCash -= 500;
+        GameManager.internalCash -= _upgradeCost;
+        _gm.KeyPresses();
+        _upgradeCost += Math.Ceiling(((_upgradeCost / 2) + _upgradeCost) * _upgradeFloat);
+        _upgradeFloat *= _upgradeFloat;
         Upgrade1(false);
     }
 }
